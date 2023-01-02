41cd48c8c02856a4ef4950a869adae64e063b2fc
gopal <gopal1409@gmail.com>
2023-01-02 08:16:55 +0530

    terraform module path

--- static-website.tf
+++ static-website.tf
@@ -1,5 +1,11 @@
 module "azure_static_website" {
-  source                            = "./terraform-manifest/modules/terraform-static-website" #mandatory
+  #calling local module
+  #source                            = "./terraform-manifest/modules/terraform-static-website" #mandatory
+  #calling the module from terraform registry
+  #source  = "gopal1409/staticwebsite/azurerm"
+  #version = "1.0.0"
+  #clone the module over https
+  #source = "git::https://github.com/gopal1409/terraform-azurerm-staticwebsite.git:ref=1.0.0"
   location                          = "eastus"
   resource_group_name               = "myrg1"
   storage_account_name              = "staticwebsite"
